00d14591238efb34a6b1db627e73c7aac876b8e3
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-03 00:50:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(new_name)
+    new_song = Song.new
+    new_song.name = new_name
+    new_song
+  end
+
+  def self.create_by_name(new_name)
+    new_song = Song.new
+    new_song.name = new_name
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(find_name)
+    Song.all.detect do |song|
+      song.name == find_name
+    end
+  end
+
+  def self.find_or_create_by_name(fc_name)
+    if Song.find_by_name(fc_name)!= nil
+      Song.find_by_name(fc_name)
+    else
+      Song.create_by_name(fc_name)
+    end
+  end
 end
